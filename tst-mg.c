6dae6643f58ae0cc1489ee98ca42ce63dd9a2070
ZhengYihao <79893019+zhg021@users.noreply.github.com>
2024-10-28 13:29:37 -0700

    Update tst-mg.c

--- tst-mg.c
+++ tst-mg.c
@@ -1,41 +1,28 @@
 #include <stdio.h>
-#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
 
-// definir el GPIO donde esta conectado el sensor 3144E
-#define HALL_SENSOR_PIN GPIO_NUM_4
-// definir el GPIO para un LED (opcional)
-#define LED_PIN GPIO_NUM_2
+#define INTERRUPT_PIN GPIO_NUM_13
 
-void app_main(void)
-{
-    // configuracion del GPIO para el sensor de efecto Hall
-    gpio_config_t io_conf;
-    io_conf.intr_type = GPIO_INTR_DISABLE;   // no usar interrupciones
-    io_conf.mode = GPIO_MODE_INPUT;          // establecer como entrada
-    io_conf.pin_bit_mask = (1ULL << HALL_SENSOR_PIN); // pin del sensor
-    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE; // deshabilitar pull-down
-    io_conf.pull_up_en = GPIO_PULLUP_ENABLE;   // habilitar pull-up
-    gpio_config(&io_conf);
+uint8_t one_time1 = 1;
+uint8_t one_time2 = 1;
 
-    // configuracion del GPIO para el LED (opcional)
-    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+void set_pins();
+void read_door();
 
-    while (1) {
-        // leer el valor del sensor
-        int hall_sensor_value = gpio_get_level(HALL_SENSOR_PIN);
-
-        // comprobar si el sensor detecta un campo magnetico
-        if (hall_sensor_value == 0) {
-            printf("Campo magnetico detectado\n");
-            gpio_set_level(LED_PIN, 1); // encender LED
-        } else {
-            printf("No se detecta campo magnetico\n");
-            gpio_set_level(LED_PIN, 0); // apagar LED
-        }
-
-        // esperar 500 ms antes de leer de nuevo
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
+void app_main(void) {
+    
+    set_pins();
+    xTaskCreate(&read_door, "lectura_puerta", 1024, NULL, 5, NULL);
+    
 }
+
+void set_pins(){
+    gpio_reset_pin(INTERRUPT_PIN);
+    //gpio_pad_select_gpio(INTERRUPT_PIN);
+    gpio_set_direction(INTERRUPT_PIN, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(INTERRUPT_PIN, GPIO_PULLUP_ONLY);
+}
+
+void read_door(){
